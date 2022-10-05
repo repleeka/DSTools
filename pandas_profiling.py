2339e68132f61b2805cedc99a3b67e66c8380379
repleeka <tungondugi@gmail.com>
2022-10-05 16:53:44 +0530

    pandas profile report generator

---
+++ pandas_profiling.py
@@ -0,0 +1,13 @@
+# Pandas Profiling package can generate report of any data
+# in just a few minutes(according to the dataset size)
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+df = pd.read_csv('Colon.csv', engine='python')
+cols = df.columns[0:1990]
+df.drop(cols, axis=1, inplace=True)
+df
+
+# Generating Report as an HTML File
+
+ProfileReport(df).to_file(output_file="colonAnalysis.html")
